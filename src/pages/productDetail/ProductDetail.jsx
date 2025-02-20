2c922da1e2c48683118376c0d488899561e30d4f
Marco <marko.barraza@gmail.com>
2025-02-20 00:30:26 -0300

    css agregado a la card

--- src/pages/productDetail/ProductDetail.jsx
+++ src/pages/productDetail/ProductDetail.jsx
@@ -7,19 +7,31 @@ export default function ProductDetail() {
   return (
     <>
         <div className="containerProduct" >
-            <h1 className="titleProduct">Cafetera Italiana Simple Cook Volterra 6 tazas</h1>
-            <p className="descriptionProduct">La cafetera italiana Siena de Simple Cook destaca por su diseño elegante con asa de madera y cuerpo de aluminio lacado. Compatible con todas las encimeras, ofrece mayor presión para un café más intenso.</p>
-            <img src="/market_cafe/src/assets/header-cafeteras.png" className="imageProduct"   alt=""/>
-            <p className="priceProduct" >$ 16.990</p>
+            <div className="containerProduct_img">
+                <img src="/src/assets/img-prueba.jpg" className="imageProduct"   alt=""/>
+            </div>
 
-            <div className="addProduct">
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-primary">-</button>
-                    <p>0</p>
-                    <button type="button" class="btn btn-primary">+</button>
+            <div className="containerProduct_info">
+                <h2 className="titleProduct">
+                    Cafetera Italiana Simple Cook Volterra 6 tazas
+                </h2>
+                <p className="descriptionProduct">
+                    La cafetera italiana Siena de Simple Cook destaca por su 
+                    diseño elegante con asa de madera y cuerpo de aluminio lacado. 
+                    Compatible con todas las encimeras, ofrece mayor presión para 
+                    un café más intenso.
+                </p>
+                <p className="priceProduct" >$ 16.990</p>
+
+                <div className="addProduct">
+                        <a href=""  className="addProduct_button">Añadir</a>    
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-primary btn-addProduct">-</button>
+                        <p>0</p>    
+                        <button type="button" class="btn btn-primary btn-addProduct">+</button>
+                    </div>
                 </div>
-                    <button variant="success" >Add To Cart</button>
-                </div>
+            </div>
         </div>
 
     </>
